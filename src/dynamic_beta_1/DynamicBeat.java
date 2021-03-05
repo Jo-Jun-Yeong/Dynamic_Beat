e940b5359847eabd41429eb3de78510d73c87cbc
idwns <idwns@192.168.200.141>
2021-03-05 15:22:22 +0900

    게임 실행시 창 구현

---
+++ src/dynamic_beta_1/DynamicBeat.java
@@ -0,0 +1,14 @@
+package dynamic_beta_1;
+
+import javax.swing.JFrame;
+
+public class DynamicBeat extends JFrame {
+	public DynamicBeat() { 	//생성자 : 클래스와 같은이름을 가지고있는 메소드
+		setTitle("Dynamic Beat");
+		setSize(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
